bcae64b16bd559a358924e5ecbaa3a5c93cc1891
Marcin Kozlowski <marcinguy@gmail.com>
2020-07-10 11:14:42 +0200

    Added AUTHORS

--- fuzzer.c
+++ fuzzer.c
@@ -1,4 +1,15 @@
-// gcc dragonfly.c ptrace_helpers.c snapshot.c -o dragonfly
+/*
+ @Author    : h0mbre, marcinguy
+ @date      : July 2020
+
+Permission to use, copy, modify, distribute, and sell this software and its
+documentation for any purpose is hereby granted without fee, provided that
+the above copyright notice appear in all copies and that both that
+copyright notice and this permission notice appear in supporting
+documentation.  No representations are made about the suitability of this
+software for any purpose.  It is provided "as is" without express or
+implied warranty.
+*/
 
 #define _GNU_SOURCE
 #include <stdio.h>
